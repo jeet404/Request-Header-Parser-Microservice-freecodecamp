45af626786a5be9992f6f76faff9e524085c4466
jeetuw <jeet63240@gmail.com>
2023-08-17 10:39:12 +0530

    Change IP key name

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/api/whoami", (req, res) => {
   const ip = req.ip;
   const language = req.headers["accept-language"];
   const software = req.headers["user-agent"];
-  res.send({ ip: ip, language: language, software: software });
+  res.send({ ipaddress: ip, language: language, software: software });
 });
 
 // listen for requests :)
